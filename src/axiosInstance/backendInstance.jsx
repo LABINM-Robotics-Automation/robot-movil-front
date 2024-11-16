0f3db7794eff82461a76fb14444369ee461ebe37
pQbas <percybrayan18@gmail.com>
2024-11-16 12:05:52 -0500

    from url to URL

--- src/axiosInstance/backendInstance.jsx
+++ src/axiosInstance/backendInstance.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const backend = axios.create({
-  // baseUrl: `http://192.168.0.10:8000${endpoint}`, // Replace with your backend URL
-  baseUrl: `http://localhost:8000`,
+  // baseURL: `http://192.168.0.10:8000${endpoint}`, // Replace with your backend URL
+  baseURL: `http://localhost:8000`,
   // baseUrl: `http://127.0.0.1:8000${endpoint}`,
 })
 
